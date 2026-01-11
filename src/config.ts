77bdb389993e78147788199ed50c0e18f78a4e8f
FateGodI <cnop_six_cloud@outlook.com>
2026-01-12 06:38:18 +0800

    添加博客文章和相关配置

--- src/config.ts
+++ src/config.ts
@@ -48,16 +48,16 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // 您可以替换为自己的头像
-	name: "404 Blog", // 修改为您的名字或昵称
-	bio: "技术探索、代码分享与日常思考", // 更新个人简介
-	links: [
-		{
-			name: "GitHub",
-			icon: "fa6-brands:github", // GitHub图标
-			url: "https://github.com/FateGodI", // 您的GitHub主页地址
-		},
-		// 可以根据需要添加更多社交链接，例如：
+	avatar: "assets/images/avatar.jpg", // 替换头像
+	name: "404 Blog", // 修改昵称
+	bio: "技术探索、代码分享与日常思考", // 个人简介
+	//links: [
+	//	{
+	//		name: "GitHub",
+	//		icon: "fa6-brands:github", // GitHub图标
+	//		url: "https://github.com/FateGodI", // GitHub主页地址
+	//	},
+		// 更多社交链接，例如：
 		// {
 		// 	name: "Twitter",
 		// 	icon: "fa6-brands:twitter",
@@ -65,10 +65,10 @@ export const profileConfig: ProfileConfig = {
 		// },
 		// {
 		// 	name: "Email",
-		// 	icon: "fa6-solid:envelope",
+		// 	icon: "fa6-solid:envelope",s
 		// 	url: "mailto:support@404bbs.com",
 		// },
-	],
+	//],
 };
 
 export const licenseConfig: LicenseConfig = {
