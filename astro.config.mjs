6e688d132e56aea1ca35306f0e0908e3bf383632
zkee <135090201+FateGodI@users.noreply.github.com>
2026-01-12 04:17:42 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "http://blog.404bbs.com",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
